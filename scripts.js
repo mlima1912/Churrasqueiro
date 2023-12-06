fee5604d7c5882fdcdf723391f8cf6da2d34d273
Marcos de Lima <125506852+mlima1912@users.noreply.github.com>
2023-12-06 11:30:07 -0300

    churrasqueiro

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Adicione aqui o código para processar o formulário ou enviar um pedido de orçamento
+    alert('Formulário enviado com sucesso!');
+});
